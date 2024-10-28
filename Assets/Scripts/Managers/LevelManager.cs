81ab12af67a3da6e9604402371a4c1302c6179e2
Emma <pawful@bell.net>
2024-10-28 14:35:15 -0400

    added some highscore logic

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -15,7 +15,10 @@ public class LevelManager : MonoBehaviour
     //for a loading progress bar
     [SerializeField] private Image _progressBar;
 
+    //defining my keys
     private const string CompletedLevelKey = "CompletedLevel_";
+    private const string HighScoreKey = "HighScore";
+
     void Awake()
     {
        if(Instance == null)
@@ -31,27 +34,24 @@ public class LevelManager : MonoBehaviour
 
     public async void LoadScene(string sceneName)
     {
-        //if (IsLevelUnlocked(sceneName))
-        //{
-            var scene = SceneManager.LoadSceneAsync(sceneName);
-            scene.allowSceneActivation = false;
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
 
-            //set the loader canvas to true
-            _loaderCanvas.SetActive(true);
+        //set the loader canvas to true
+         _loaderCanvas.SetActive(true);
 
-            //see how much the scene has loaded and change the fill amount
-            do
-            {
+         //see how much the scene has loaded and change the fill amount
+         do
+         {
                 //basically this updates every .1 seconds and fills the porgress bar
-                await Task.Delay(1000);
-                _progressBar.fillAmount = scene.progress;
-            }
-            while (scene.progress < 0.9f);
+             await Task.Delay(1000);
+             _progressBar.fillAmount = scene.progress;
+         }
+         while (scene.progress < 0.9f);
 
-            scene.allowSceneActivation = true;
-            //disable the loader canvas
-            _loaderCanvas.SetActive(true);
-        //}
+         scene.allowSceneActivation = true;
+         //disable the loader canvas
+         _loaderCanvas.SetActive(true);
     }
 
     //player prefs save small bits of data
@@ -66,18 +66,19 @@ public class LevelManager : MonoBehaviour
         return PlayerPrefs.GetInt(CompletedLevelKey + levelName, 0) == 1;
     }
 
-    //public bool IsLevelUnlocked(string levelName)
-    //{
-    //    if (levelName == "Level1") 
-    //        return true; // Level 1 is always unlocked
+    public void SetHighScore(int score)
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
 
-    //    string previousLevel = GetPreviousLevelName(levelName);
-    //    return IsLevelComplete(previousLevel);
-    //}
+        if(score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, score);
+            PlayerPrefs.Save();
+        }
+    }
 
-    //private string GetPreviousLevelName(string currentLevel)
-    //{
-    //    int levelNumber = int.Parse(currentLevel.Replace("Level", ""));
-    //    return $"Level{levelNumber - 1}";
-    //}
+    public int GetHighScore()
+    {
+        return PlayerPrefs.GetInt(HighScoreKey, 0);
+    }
 }
