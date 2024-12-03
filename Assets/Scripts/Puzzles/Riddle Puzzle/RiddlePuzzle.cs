5265b45497ed601f4d9f6b93fd067c1e3fee5c4d
Emma <pawful@bell.net>
2024-12-03 13:55:48 -0500

    finished the riddle puzzle

---
+++ Assets/Scripts/Puzzles/Riddle Puzzle/RiddlePuzzle.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RiddlePuzzle : MonoBehaviour
+{
+    #region Variables
+
+    public TMP_InputField inputField; 
+    public TextMeshProUGUI riddleText; 
+    public TextMeshProUGUI feedbackText; 
+    
+    public PuzzleTrigger puzzleTrigger;
+    public PuzzleManager puzzleManager;
+    public PlayerBehaviour player;
+    public MainCamera cameraPitchRef;
+    public GameObject puzzleUI;
+    
+    [SerializeField]
+    public string correctAnswer = "2"; 
+
+    [SerializeField]
+    public string riddle = "I am nothing, but without me, everything would be incomplete. What am I?";
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (riddleText != null)
+        {
+            riddleText.text = riddle;
+        }
+
+        if (feedbackText != null)
+        {
+            feedbackText.text = "";
+        }
+    }
+
+    public void CheckAnswer()
+    {
+        if (inputField.text == correctAnswer)
+        {
+            HandleCorrectAnswer();
+        }
+        else
+        {
+            HandleIncorrectAnswer();
+        }
+    }
+
+    private void HandleCorrectAnswer()
+    {
+        feedbackText.text = "The correct number is " + correctAnswer;
+        feedbackText.color = Color.green;
+
+        StartCoroutine(DisplayYouWinText());
+
+        cameraPitchRef.isCameraLocked = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        puzzleTrigger.PuzzleCompleted();
+        inputField.interactable = false;
+    }
+
+    private IEnumerator DisplayYouWinText()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        if (puzzleUI != null)
+        {
+            puzzleUI.SetActive(false);
+        }
+
+        if (puzzleManager != null)
+        {
+            puzzleManager.CompletePuzzle();
+        }
+    }
+
+    private void HandleIncorrectAnswer()
+    {
+        feedbackText.text = "Incorrect answer. Try again!";
+        feedbackText.color = Color.red;
+    }
+}
